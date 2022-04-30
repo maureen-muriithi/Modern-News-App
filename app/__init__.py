a10e226634f182b169c2fffb156578279e8816ba
Maureen-Muriithi <moh2wanja@gmail.com>
2022-04-30 03:15:36 +0300

    Add API Key and add the virtual and the key to gitignore

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,10 @@
 from flask import Flask
+from .config import DevConfig
 
 # Initializing application
 app = Flask(__name__)
+
+#Configuration settings
+app.config.from_object(DevConfig)
 
 from app import views
--- run.py
+++ run.py
@@ -2,4 +2,4 @@ from distutils.log import debug
 from app import app 
 
 if __name__ == '__main__':
-    app.run(debug = True)
+    app.run()
