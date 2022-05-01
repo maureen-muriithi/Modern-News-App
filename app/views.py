29add0613a0c9f2bccfc9e8740f50291e09b9afa
Maureen-Muriithi <moh2wanja@gmail.com>
2022-05-01 23:59:23 +0300

    Dry code with macros

--- app/views.py
+++ app/views.py
@@ -7,7 +7,8 @@ def headers():
 
     # Getting popular news
     us_news = get_news('us')
+    de_news = get_news('de')
     print(us_news)
     title = 'Home page for the News App'
     heading = "Welcome to your Favorite Modern News App"
-    return render_template('index.html', title = title, heading = heading, us = us_news)
+    return render_template('index.html', title = title, heading = heading, us = us_news, de = de_news)
